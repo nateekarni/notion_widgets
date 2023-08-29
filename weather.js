455ab476b7d9087e9e8a4e06be671e2e09e44c42
Nateekarn Sukying <73416871+nateekarni@users.noreply.github.com>
2023-08-29 21:15:53 +0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,33 +1,14 @@
-document.body.addEventListener('load',getWeather());
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(getWeather, locationError);
-} else {
-  showError("Your browser does not support Geolocation!");
+function getData(){
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=13.73202006720473&lon=100.4670411787257&appid=093d0d2110801e734e3a6ff0b2f346af", {
+    method: "GET"
+  }).then((response) => response.json()).then((data) => {
+  console.log(data);
+  locate.innerHTML = data.name + ", " + data.sys.country;
+  temp.innerHTML = Math.round((data.main.temp-32)/9*100)/100 + "°C";
+  weather_status.innerHTML = data.weather[0].main;
+  windspeed.innerHTML = data.wind.speed + " km/hr";
+  humidity.innerHTML = Math.round((data.main.humidity)*100)/100 + "%";
+})
 }
 
-function getWeather(position){
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-  var acc = position.coords.accuracy;
-  const API_KEY = '093d0d2110801e734e3a6ff0b2f346af'
-
-  console.log(position.coords);
-  console.log("Accuracy: "+acc+"\nLatitude: "+lat+"\nLongitude: "+lon);
-
-  let weather_show = document.getElementById("weather_status");
-  let temp_show = document.getElementById("temp");
-  let local = document.getElementById("locate");
-
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`).then(response => response.json()).then((response) => {
-    console.log(data)
-    weather_show.innerHTML = `${response.main}`;
-    temp_show.innerHTML = `${response.main.temp}`;
-    local.innerHTML = `${response.sys.name}`;
-  });
-}
-
-function keepPosition(){
-  latitude_pos = position.coords.latitude;
-  longitude_pos = position.coords.longitude;
-}
+getData();
